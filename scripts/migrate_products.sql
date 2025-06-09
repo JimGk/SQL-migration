830173395aedd84691d208e4c3b37cd9b168d792
Dimitris <Jim_gk@hotmail.com>
2025-06-10 00:35:13 +0300

    Update migrate_products.sql

--- scripts/migrate_products.sql
+++ scripts/migrate_products.sql
@@ -1,5 +1,5 @@
 
 -- Migrate products as-is
-INSERT INTO products (product_name, price)
-SELECT product_name, price
-FROM legacy_db.products;
+INSERT INTO products (product_id, product_name, price)
+SELECT product_id, product_name, price
+FROM legacy.products;
